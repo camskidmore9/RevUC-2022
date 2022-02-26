84ea1acdf5cf75b48de4ae446e8e7622cc244db0
“camskidmore9” <camskidmore2000@gmail.com>
2022-02-26 15:06:33 -0800

    Bot that responds to chat

--- Discord/bot.py
+++ Discord/bot.py
@@ -1,20 +1,77 @@
+# bot.py
 import os
 
 import discord
+import random
+import time
 from dotenv import load_dotenv
 
-#dotenv is not being used because it didn't want to work for some reason
-#Token is in plaintext for now, this will be updated later
 load_dotenv()
-TOKEN = "OTQ3MTk2MjkxNDI1ODk0NTEx.YhpvSQ.MIkprnp3bJJ7gz7xcZ5yYRecPPI"#os.getenv('DISCORD_TOKEN')
+TOKEN = "OTQ3MTk2MjkxNDI1ODk0NTEx.YhpvSQ.5CJjJhJsA5C2U_I82PaYbShWPCY"#os.getenv('DISCORD_TOKEN')
 
-#defines the client that will be called
+
 client = discord.Client()
 
-#Creates the event and defines what it will do
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{client.user.name} has connected to Discord!')
 
-#Runs the client and connects the bot to discord. When this runs, the bot will appear 'online' on discord
+'''
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+'''
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'Balls. Nuts even!'
+    ]
+
+    if message.content == 'balls?':
+        response = random.choice(brooklyn_99_quotes)
+        time.wait(3)
+        await message.channel.send(response)
+
+
+
 client.run(TOKEN)
+
+'''
+
+
+class CustomClient(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+
+client = CustomClient()
+client.run(TOKEN)
+'''
+
+'''
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+client.run(TOKEN)
+'''
